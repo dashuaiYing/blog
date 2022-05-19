a48530b1b98714830768ee0191ede8f9126eabba
xiaoming <tr_ymj@qq.com>
2022-05-19 11:10:36 +0800

    音乐插件和看板

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -20,6 +20,54 @@ module.exports = {
     ]
   ],
   "theme": "reco",
+  "plugins": [
+    [
+      //先安装在配置， npm install @vuepress-reco/vuepress-plugin-kan-ban-niang --save
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      {
+        theme: ['koharu', 'blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        clean: false,
+        messages: {
+          welcome: '我是xiaoming欢迎你的关注 ',
+          home: '心里的花，我想要带你回家。',
+          theme: '好吧，希望你能喜欢我的其他小伙伴。',
+          close: '再见哦'
+        },
+        width: 260,
+        height: 352,
+      }
+    ],
+    [
+      '@vuepress-reco/vuepress-plugin-bgm-player',
+      {
+        audios: [
+          {
+            name: '一路生花',
+            artist: 'xiaoming',
+            url: '/温奕心-一路生花.mp3',
+            cover: '/avatar.png'
+          }
+        ] ,
+        // 是否默认缩小
+        autoShrink: true ,
+        // 缩小时缩为哪种模式
+        shrinkMode: 'float',
+        // 悬浮窗样式
+        floatStyle:{ bottom: '20px', 'z-index': '999999' }
+      }
+    ],
+    [
+      '@vuepress/plugin-medium-zoom', {
+        selector: '.page img',
+        delay: 1000,
+        options: {
+          margin: 24,
+          background: 'rgba(25,18,25,0.9)',
+          scrollOffset: 40
+        }
+      }
+    ],
+  ],
   "themeConfig": {
     "nav": [
       {
